7d6783dff8382e54c4095887c1b70268f829a09a
Brandon Heat <stardustgrave100msn@ybb.ne.jp>
2023-05-30 21:55:55 +0900

    【A4.2】Devise継承ユーザーＣの為の共通モジュール生成＆アカウント作成ビューとアカウント編集ビューの初期実装

---
+++ app/controllers/concerns/commons.rb
@@ -0,0 +1,42 @@
+module Commons
+  
+  # beforeフィルター
+
+  # paramsハッシュからユーザーを取得します。
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  # ログイン済みのユーザーか確認します。
+  def logged_in_user
+    unless logged_in?
+      store_location
+      flash[:danger] = "ログインしてください。"
+      redirect_to login_url
+    end
+  end
+
+  # アクセスしたユーザーが現在ログインしているユーザーか確認します。
+  def correct_user
+    unless current_user?(@user)
+      flash[:danger] = "本人アカウントでログインしてください。"
+      redirect_to(root_url)
+    end
+  end
+
+  # システム管理権限所有かどうか判定します。
+  def admin_user
+    unless current_user.admin?
+      flash[:danger] = "システム管理権限がありません。"
+      redirect_to root_url
+    end
+  end
+
+  # 管理権限者、または現在ログインしているユーザー本人を許可します。
+  def admin_or_correct_user
+    unless current_user?(@user) || current_user.admin?
+      flash[:danger] = "権限がありません。"
+      redirect_to(root_url)
+    end
+  end
+end
--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,62 +1,66 @@
-# frozen_string_literal: true
-
 class Users::RegistrationsController < Devise::RegistrationsController
-  # before_action :configure_sign_up_params, only: [:create]
-  # before_action :configure_account_update_params, only: [:update]
+  include Commons
 
-  # GET /resource/sign_up
-  # def new
-  #   super
-  # end
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :logged_in_user, only: [:index, :show, :edit, :update, :destroy]
+  before_action :correct_user, only: [:show, :edit, :update]
+  before_action :admin_user, only: [:index, :destroy]
+  before_action :admin_or_correct_user, only: :destroy
+  
+  def index
+    @users = User.paginate(page: params[:page], per_page: 5)
+  end
+  
+  def show
+  end
 
-  # POST /resource
-  # def create
-  #   super
-  # end
+  def new
+    if logged_in? && !current_user.admin?
+      flash[:info] = 'すでにログインしています。'
+      redirect_to current_user
+    end
+    @user = User.new
+  end
+  
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      log_in @user
+      flash[:success] = 'ユーザーの新規作成に成功しました。'
+      redirect_to @user
+    else
+      render :new
+    end
+  end
+  
+  def edit
+  end
+  
+  def update
+    if  @user.update_attributes(user_params)
+      flash[:success] = "ユーザー情報を更新しました。"
+      redirect_to @user
+    else
+      render :edit
+    end
+  end
 
-  # GET /resource/edit
-  # def edit
-  #   super
-  # end
+  def destroy
+    @user.destroy
+    flash[:success] = "#{@user.name}のデータを削除しました。"
+    redirect_to users_url
+  end
 
-  # PUT /resource
-  # def update
-  #   super
-  # end
-
-  # DELETE /resource
-  # def destroy
-  #   super
-  # end
-
-  # GET /resource/cancel
-  # Forces the session data which is usually expired after sign
-  # in to be expired now. This is useful if the user wants to
-  # cancel oauth signing in/up in the middle of the process,
-  # removing all OAuth session data.
-  # def cancel
-  #   super
-  # end
-
-  # protected
-
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_up_params
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
-  # end
-
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_account_update_params
-  #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
-  # end
-
-  # The path used after sign up.
-  # def after_sign_up_path_for(resource)
-  #   super(resource)
-  # end
-
-  # The path used after sign up for inactive accounts.
-  # def after_inactive_sign_up_path_for(resource)
-  #   super(resource)
-  # end
+  private
+  
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+    
+    # ここから下はユーザーＣ専用のbeforeフィルター
+    
+    # paramsハッシュからユーザーを取得します。
+    def set_user
+      @user = User.find(params[:id])
+    end
 end
