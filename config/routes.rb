e30920d9c2834d2d3a6c322b528c21ddd4d23a92
Brandon Heat <stardustgrave100msn@ybb.ne.jp>
2023-05-31 11:12:09 +0900

    【A4.5】ルーティング設定でユーザーindexアクションとユーザーshowアクションの生成

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,8 @@ Rails.application.routes.draw do
     omniauth_callbacks: "omniauth_callbacks",
     registrations: 'users/registrations'
   }
+
+  get 'users', to: 'users/registrations#index' # ユーザーindexアクションの生成
+  get 'users/:id', to: 'users/registrations#show' # ユーザーshowアクションの生成
+
 end
