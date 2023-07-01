cfde9c1ebb3fbc5c2c70cd07257d90bd46197deb
Brandon Heat <stardustgrave100msn@ybb.ne.jp>
2023-07-01 11:05:44 +0900

    【A7】ユーザー系、ログイン以外の機能面の基本実装＆Devise機能面の日本語化（LINEログイン復活は後回し）

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     sessions: 'users/sessions',
     registrations: 'users/registrations',
-    omniauth_callbacks: 'users/omniauth_callbacks'
+    omniauth_callbacks: 'omniauth_callbacks'
   }
 
   resources :users, only: [:index, :show, :destroy]
