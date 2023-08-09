896f9c9a9fc500809df343c1e1161e4e3e220919
Brandon Heat <stardustgrave100msn@ybb.ne.jp>
2023-08-10 07:48:11 +0900

    【A11.20】LINEログイン機能に実装を戻し、OpenIDconnectに申請

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
-         :omniauthable, omniauth_providers: [:facebook] # Facebookログイン用の連携設定を追加。
-         # omniauth_providers: %i[line] # LINEログイン用の連携設定を追加。
+         :omniauthable,
+         omniauth_providers: %i[line] # LINEログイン用の連携設定を追加。
+         # omniauth_providers: [:facebook] # Facebookログイン用の連携設定を追加。
          
   
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -18,22 +19,22 @@ class User < ApplicationRecord
   validates :password, presence: true, length: { minimum: 6 }, on: :create
   # validates_confirmation_of :password
 
-  def self.find_for_oauth(auth)
-    user = User.where(uid: auth.uid, provider: auth.provider).first
+  # def self.find_for_oauth(auth)
+    # user = User.where(uid: auth.uid, provider: auth.provider).first
 
-    unless user
-      user = User.create(
-        uid:      auth.uid,
-        provider: auth.provider,
-        email:    auth.info.email,
-        name:  auth.info.name,
-        password: Devise.friendly_token[0, 20]
+    # unless user
+      # user = User.create(
+        # uid:      auth.uid,
+        # provider: auth.provider,
+        # email:    auth.info.email,
+        # name:  auth.info.name,
+        # password: Devise.friendly_token[0, 20]
         # , image:  auth.info.image
-      )
-    end
+      # )
+    # end
 
-    user
-  end
+    # user
+  # end
 
   def update_without_current_password(params, *options)
     params.delete(:current_password)
@@ -48,25 +49,25 @@ class User < ApplicationRecord
     result
   end
 
-  # def social_profile(provider)
-    # social_profiles.select { |sp| sp.provider == provider.to_s }.first
-  # end
+  def social_profile(provider)
+    social_profiles.select { |sp| sp.provider == provider.to_s }.first
+  end
 
-  # def set_values(omniauth)
-    # return if provider.to_s != omniauth["provider"].to_s || uid != omniauth["uid"]
-    # credentials = omniauth["credentials"]
-    # info = omniauth["info"]
+  def set_values(omniauth)
+    return if provider.to_s != omniauth["provider"].to_s || uid != omniauth["uid"]
+    credentials = omniauth["credentials"]
+    info = omniauth["info"]
 
-    # access_token = credentials["refresh_token"]
-    # access_secret = credentials["secret"]
-    # credentials = credentials.to_json
-    # name = info["name"]
-  # end
+    access_token = credentials["refresh_token"]
+    access_secret = credentials["secret"]
+    credentials = credentials.to_json
+    name = info["name"]
+  end
 
-  # def set_values_by_raw_info(raw_info)
-    # self.raw_info = raw_info.to_json
-    # self.save!
-  # end
+  def set_values_by_raw_info(raw_info)
+    self.raw_info = raw_info.to_json
+    self.save!
+  end
   
   def admin?
     admin # adminカラムがtrueの場合を管理者とする
